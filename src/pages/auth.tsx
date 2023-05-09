4063cead9f38b4dae62603cf71eadeca9e92c7d7
Alirezarostami <rostamialireza42@gmail.com>
2023-05-09 12:19:15 +0430

    Test

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -19,7 +19,7 @@ export default function auth({
   return (
     <div className="w-full flex items-center justify-center">
       <div className="w-full h-100 flex items-center justify-center">
-        {/*----Form----*/}
+        {/*----Form here----*/}
         <div className="w-full sm:w5/6 md:w-2/3 lg:w1/2 xl:w-1/3 2xl:w-1/3 h-full bg-white flex flex-col items-center justify-center">
           {tab == "signin" ? (
             <LoginForm callbackUrl={callbackUrl} csrfToken={csrfToken} />
